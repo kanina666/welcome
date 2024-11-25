5c39b47aba914096593314f5c5a2e2a280f75872
Конькова Нина Александровна <ninakonkova19@yandex.ru>
2024-11-25 23:30:29 +0300

    +

--- data_easy.py
+++ data_easy.py
@@ -6,10 +6,11 @@ best_t = None
 
 for t in [point[0] for point in data]:
     errors = sum(1 for x, y in data if (x < t and y == 1) or (x >= t and y == 0))
-    # Условие обновления: строго меньшее количество ошибок или минимальный индекс при равных ошибках
+    # Условие обновления:строго меньшее количество ошибок или минимальный индекс при равных ошибках
     if errors < min_errors or (errors == min_errors and (best_t is None or t < best_t)):
         min_errors = errors
         best_t = t
 
 print(best_t, min_errors)
+
 
